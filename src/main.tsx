94e6aa3a376615e41cc1fd65e798e4a1e350f8f5
Oksana-Bil25 <vikos24g@gmail.com>
2025-12-01 22:07:25 +0200

    Fix: New line ending configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./components/App/App";
-// Імпортуйте modern-normalize, якщо він встановлений
-// import 'modern-normalize/modern-normalize.css';
 import "./index.css";
 
 const queryClient = new QueryClient({
